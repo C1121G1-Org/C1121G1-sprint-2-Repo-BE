473c59e36c2d25e49301620d1dfb79426c5870a6
nguyenquoctuan110398 <nguyenquoctuan11031998@gmail.com>
2022-07-04 17:25:03 +0700

    merge dev

--- sprint_2_webservice/src/main/java/api/controllers/PostRestController.java
+++ sprint_2_webservice/src/main/java/api/controllers/PostRestController.java
@@ -1,35 +1,81 @@
 package api.controllers;
 
+import api.dto.PostDto;
+import api.models.Guest;
+import api.models.Post;
 import api.models.ResponseObject;
+import api.services.IGuestService;
 import api.services.IPostService;
+import api.services.impl.PostServiceImpl;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/api/post")
 public class PostRestController {
     @Autowired
-    IPostService iPostService;
+    private IPostService iPostService;
 
+    @Autowired
+    private IGuestService iGuestService;
+
+    /*
+        Created by TuanNQ
+        Time: 14:00 23/06/2022
+        Function: Create post
+    */
     @PostMapping(value = "/create")
-    public ResponseEntity<ResponseObject> createPost(){
-        return null;
+    public ResponseEntity<ResponseObject> createPost(@Valid @RequestBody PostDto postDto,
+                                                     BindingResult bindingResult) {
+        Map<String, String> errorMap = new HashMap<>();
+        if (bindingResult.hasErrors()) {
+            bindingResult
+                    .getFieldErrors()
+                    .stream()
+                    .forEach(f -> errorMap.put(f.getField(), f.getDefaultMessage()));
+            return new ResponseEntity<>(new ResponseObject(false, "Failed!",
+                    errorMap, new ArrayList()), HttpStatus.BAD_REQUEST);
+        } else {
+            Post post = new Post();
+            BeanUtils.copyProperties(postDto, post);
+
+            Guest guest = iGuestService.findGuestById(postDto.getGuestId());
+
+            if (guest != null) {
+                post.setGuest(guest);
+
+                if (!postDto.getContent().trim().equals("")) {
+                    post.setContent(postDto.getContent());
+                }
+                iPostService.createPost(post);
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+        }
+
+        return new ResponseEntity<>(new ResponseObject(false, "Failed!", errorMap,
+                new ArrayList<>()), HttpStatus.BAD_REQUEST);
     }
 
     @GetMapping(value = "/list")
-    public ResponseEntity<ResponseObject> listPost(){
-        return null;
-    }
-
-    @PatchMapping(value = "/update/{id}")
-    public ResponseEntity<ResponseObject> updatePost(@PathVariable String id){
+    public ResponseEntity<ResponseObject> listPost() {
         return null;
     }
 
     @PatchMapping(value = "/delete/{id}")
-    public ResponseEntity<ResponseObject> deletePost(@PathVariable String id){
+    public ResponseEntity<ResponseObject> deletePost(@PathVariable String id) {
         return null;
     } //Xóa bằng cách update lại các delete_flag = 1
 }
--- sprint_2_webservice/src/main/java/api/repositories/IPostRepository.java
+++ sprint_2_webservice/src/main/java/api/repositories/IPostRepository.java
@@ -2,7 +2,10 @@ package api.repositories;
 
 import api.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,7 +15,20 @@ import java.util.List;
 */
 public interface IPostRepository extends JpaRepository<Post, Long> {
 
-    @Query(value = " SELECT * FROM `sprint-2-db`.post where guest_id = :#{#guestId} ;",
+    /*
+        Created by TuanNQ
+        Time: 14:00 23/06/2022
+        Function: Create post
+    */
+    @Transactional
+    @Modifying
+    @Query(value = "insert into post (`image`, `post_date`, `privacy`, `feeling`, `content`, `guest_id`) " +
+            "values (:#{#post.image}, now(), ifnull(:#{#post.privacy}, 'Công khai'), :#{#post.feeling}, :#{#post.content}, :#{#post.guest.id})",
+    nativeQuery = true)
+    void createPost(Post post);
+
+    @Query(value = " SELECT * FROM `sprint-2-db`.post where guest_id = :#{#guestId} and privacy = 'Công khai'",
             nativeQuery = true)
     List<Post> findAllGuestPost(Long guestId);
+
 }
